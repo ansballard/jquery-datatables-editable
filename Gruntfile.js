c39f561e4667795083132a332f086038aeb8a919
unknown <ans.ballard@gmail.com>
2015-03-25 17:06:07 -0500

    Added grunt-contrib-connect to replace python as a dev server. Run with `grunt dev`

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,14 +30,28 @@ module.exports = function(grunt) { "use strict";
         files: ["jquery.dataTables.editable.js", ".eslintrc"],
         tasks: ["eslint"]
       }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: ""
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-eslint");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-connect");
 
   grunt.registerTask("default", ["eslint", "uglify:dist"]);
   grunt.registerTask("minify", ["uglify:dist"]);
   grunt.registerTask("lint", ["eslint"]);
+  grunt.registerTask("serve", ["connect"]);
+
+  grunt.registerTask("dev", "Lint, minify, serve index.html, and watch for changes", function() {
+    grunt.task.run(["default", "serve", "watch"]);
+  });
 };
